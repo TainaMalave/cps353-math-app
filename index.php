b2b1146ffe0ff553b199d254686a18c9ba8dd8cf
tainamalave <Taina.Malave@gmail.com>
2021-02-22 10:30:44 -0500

    basic html and css for homepage

--- index.php
+++ index.php
@@ -5,7 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trivia Game</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div class="container">
+        <div class="main-content">
+            <div class="main-title">
+                <h1>TRIVIA GAME</h1>
+            </div>
+
+            <div class="sub-title">
+                <h2>Can you beat the clock?</h2>
+            </div>
+
+            <div class="main-btns">
+                <a href="php/login.php" class="btnStyle">Login</a>
+                <a href="php/register.php" class="btnStyle">Sign Up</a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
