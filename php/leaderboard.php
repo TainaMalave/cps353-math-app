257f3fc7dd70f1babf156e768b5ed6f0716a95a4
tainamalave <Taina.Malave@gmail.com>
2021-03-26 17:50:56 -0400

    create leaderboard page

---
+++ php/leaderboard.php
@@ -0,0 +1,25 @@
+<?php
+    include('models/scoreboardModel.php');
+
+    $topTenUsers = Scoreboard::getTopTen();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leaderboard!</title>
+</head>
+<body>
+
+    <?php foreach($topTenUsers as $topTenUser) : ?>
+        <div>
+            <?php echo $topTenUser->getName() ?>
+            <?php echo $topTenUser->getScore() ?>
+        </div>
+    <?php endforeach; ?>
+    
+</body>
+</html>
