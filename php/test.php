d44739c9ba8aabaa38be519a0911ae8a09d24dce
tainamalave <Taina.Malave@gmail.com>
2021-03-26 17:53:34 -0400

    Create testing page

---
+++ php/test.php
@@ -0,0 +1,10 @@
+<?php
+include ('models/scoreboardModel.php');
+
+$user = User::getCurrentUser();
+
+$user->addScore(rand(0,12), rand(0,12));
+
+$scoreboards = Scoreboard::getTopTen();
+
+var_dump($scoreboards);
